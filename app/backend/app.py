9a7d298fd93e044ddb6f3517275cc7c508555c8a
Evyyl <85968488+Evyyl@users.noreply.github.com>
2024-10-01 11:39:49 -0300

    SQL

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_from_directory, request, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 import urllib3
 import os
+from flask_mysqldb import MySQL
 
 # Suprimindo os avisos de HTTPS não verificados
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -12,6 +14,15 @@ app_root = os.path.dirname(__file__)
 static_folder_path = os.path.join(app_root, 'frontend', 'dist')
 
 app = Flask(__name__, static_folder=static_folder_path, static_url_path='')
+CORS(app)
+
+# Configuração do MySQL
+app.config['MYSQL_HOST'] = 'localhost'  # Endereço do seu servidor MySQL
+app.config['MYSQL_USER'] = 'root'  # Usuário do MySQL (pode ser 'root' ou outro que você estiver usando)
+app.config['MYSQL_PASSWORD'] = ''  # Senha vazia
+app.config['MYSQL_DB'] = 'seu_banco_de_dados'  # Nome do seu banco de dados
+
+mysql = MySQL(app)
 
 def check_links(links):
     result = []
@@ -37,9 +48,14 @@ def check_links(links):
                     
                     result.append({"link": link, "status": "ativo", "painel": painel_name, "iframe": iframe['src']})
                     status_count["ativo"] += 1
+
+                    # Armazenando no banco de dados
+                    store_link_data(link, "ativo", painel_name, iframe['src'])
+
                 else:
                     result.append({"link": link, "status": "inválido"})
                     status_count["inválido"] += 1
+
             else:
                 result.append({"link": link, "status": f"erro {response.status_code}"})
                 status_count["erro"] += 1
@@ -50,13 +66,17 @@ def check_links(links):
     
     return result, status_count
 
+def store_link_data(link, status, painel_name, iframe_src):
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO resultados_links (link, status, painel, iframe) VALUES (%s, %s, %s, %s)", (link, status, painel_name, iframe_src))
+    mysql.connection.commit()
+    cur.close()
+
 @app.route("/")
 def index():
     try:
-        # Verifique se o caminho está correto
         print(f"Caminho para o diretório estático: {app.static_folder}")
         files = os.listdir(app.static_folder)
-        print(f"Conteúdo do diretório estático: {files}")
         if 'index.html' in files:
             return send_from_directory(app.static_folder, 'index.html')
         else:
